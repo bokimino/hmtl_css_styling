8cc6f9c454b8fb9a7317fbcba931c293a1f3292e
Bojan <boki_it@live.com>
2023-11-23 13:25:51 +0100

    created logic to edit the authors

--- edit_author.php
+++ edit_author.php
@@ -13,5 +13,29 @@ function editAuthor($pdo, $authorId, $updatedFirstName, $updatedLastName, $updat
     $query = $pdo->prepare($sql);
     return $query->execute([$updatedFirstName, $updatedLastName, $updatedBiography, $authorId]);
 }
+if (isset($_GET['id'])) {
+    $authorId = $_GET['id'];
+    $author = getAuthorById($pdo, $authorId);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['edit_author'])) {
+    // Assuming you have proper validation and sanitization
+    $authorId = $_POST['author_id'];
+    $updatedFirstName = $_POST['updated_first_name'];
+    $updatedLastName = $_POST['updated_last_name'];
+    $updatedBiography = $_POST['updated_biography'];
+
+    // Call the function to edit the author
+    if (editAuthor($pdo, $authorId, $updatedFirstName, $updatedLastName, $updatedBiography)) {
+        // Redirect to prevent form resubmission
+        header('Location: admin_dashboard.php');
+        exit();
+    } else {
+        // Handle the case where the update fails
+        echo 'Error editing author.';
+    }
+}
+?>
+
 
 
