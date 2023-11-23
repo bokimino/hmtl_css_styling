dc12f91342c2ba444b3e5e50d286eb965971096e
Bojan <boki_it@live.com>
2023-11-23 11:45:22 +0100

    created show author category function

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 function showAddCategoryForm() {
     document.getElementById('addCategoryForm').style.display = 'table-row';
 }
+function showAddAuthorForm() {
+    document.getElementById('addAuthorForm').style.display = 'table-row';
+}
