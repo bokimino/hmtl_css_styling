991c8f4061b15b5ed5bb01a6d168d96a976f72ca
Bojan <boki_it@live.com>
2023-11-25 00:35:38 +0100

    tried to fix the function confritmdelete

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -203,20 +203,5 @@
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 		<script src="./main.js"></script>
-		<script> function confirmDelete(bookId) {
-              Swal.fire({
-                  title: 'Are you sure?',
-                  text: 'This will delete the book and its associated comments.',
-                  icon: 'warning',
-                  showCancelButton: true,
-                  confirmButtonText: 'Yes, delete it!',
-                  cancelButtonText: 'Cancel'
-              }).then((result) => {
-                  if (result.isConfirmed) {
-                      document.querySelector('.delete-form input[name="book_id"]').value = bookId;
-                      document.querySelector('.delete-form').submit();
-                  }
-              });
-     }</script>	    
-	</body>
+	   </body>
 </html>
