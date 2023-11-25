eb56cd3fec69c4093d31d5ee70bd42e52879c4ea
Bojan <boki_it@live.com>
2023-11-25 18:18:51 +0100

    created LOGIN folder and created login process php file with the login for signing on the page for administrator and end users

---
+++ LOGIN/login_process.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+include __DIR__ . '/../connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT id, email, password, role_id FROM users WHERE email = ?";
+    $query = $pdo->prepare($sql);
+    $query->execute([$email]);
+    $user = $query->fetch(PDO::FETCH_ASSOC);
+
+    if ($user) {
+        if ($user['role_id'] == 1 && $password == $user['password']) {
+            $_SESSION['user_id'] = $user['id'];
+            header('Location: ../admin_dashboard.php');
+            exit();
+        }
+
+        if ($user['role_id'] == 2 && password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['user_email'] = $user['email'];  
+            $_SESSION['login_success'] = true;
+            header('Location: ../main.php?login=success');
+            exit();
+        }
+    }
+
+    $_SESSION['loginError'] = 'Invalid credentials. Please insert valid credentials.';
+    header('Location: ../main.php?login=error');
+
+    exit();
+} else {
+    header('Location: ../main.php');
+    exit();
+}
+
+
+
+
+
